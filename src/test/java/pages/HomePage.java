b226815a83b8a036315b5d2b07960f802ef994b8
t_klimova@inbox.ru <t_klimova@inbox.ru>
2022-12-01 13:08:51 +0300

    with corrections ver3.2

--- src/test/java/pages/HomePage.java
+++ src/test/java/pages/HomePage.java
@@ -3,8 +3,6 @@ package pages;
 
 import com.codeborne.selenide.SelenideElement;
 
-import java.time.Duration;
-
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$;
